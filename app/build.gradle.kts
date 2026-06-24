2314ac0f26961e89ca2222fe4a6f870df4c19bb0
test-forge-x <testforgex@gmail.com>
2026-06-24 16:38:07 +0530

    Bump app version to 1.4 and increment version code to 5.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.pixeleye.plantdoctor"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GEMINI_API_KEY", "\"${localProperties.getProperty("GEMINI_API_KEY", "")}\"")
